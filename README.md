be3a3c92795527293216a4b3724ee69f53729eae
Dan Pilch <daniel.pilch@ppluk.com>
2016-10-06 14:41:43 +0100

    added majority of webdriver logic

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # tickets
+
+## Installation
+
+pip install -r requirements.txt
+
+## Usage
+
+python tickets.py -h
---
+++ tickets.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+import threading
+import argparse
+import os
+
+
+class GlastonburyTickets(threading.Thread):
+    def __init__(self, base_url, proxy_file):
+        self.threads = []
+        self.base_url = base_url
+        self.proxy_list = [line.strip() for line in open(proxy_file, 'r')]
+
+    def proxy_manager(self):
+        pass
+
+    def browser(self, proxy, base_url):
+        print "Running FF instance for proxy: {0}".format(proxy)
+        driver = webdriver.Firefox()
+        driver.get(base_url)
+
+    def spawn_browsers(self):
+        for proxy in self.proxy_list:
+            t = threading.Thread(target=self.browser, args=(proxy, self.base_url,))
+            self.threads.append(t)
+            t.start()
+
+def main():
+
+    # Get current directory
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    # Argument parser
+    parser = argparse.ArgumentParser(description="Attempt to get past the great Glastonbury wait")
+    parser.add_argument("--base-url", "-u", help="Glastonbury URL", default="http://glastonbury.seetickets.com")
+    parser.add_argument("--proxy-file", "-f", help="Path to proxy list", default=dir_path+"/proxy_list.txt")
+    args = parser.parse_args()
+
+    # Start the process
+    tickets = GlastonburyTickets(base_url=args.base_url, proxy_file=args.proxy_file)
+    tickets.spawn_browsers()
+
+
+if __name__ == "__main__":
+    main()
