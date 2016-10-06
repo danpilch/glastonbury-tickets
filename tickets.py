df41f11cc9c9eef3a9179e1c7474844ef1c1f85f
Lewis Barber <lewis_barber@hotmail.com>
2016-10-06 20:25:11 +0100

    Counts success rate loading each proxy

--- tickets.py
+++ tickets.py
@@ -3,35 +3,60 @@ import threading
 import argparse
 import os
 
-
 class GlastonburyTickets(threading.Thread):
 
     def __init__(self, base_url, proxy_file):
         self.threads = []
         self.base_url = base_url
         self.proxy_list = [line.strip() for line in open(proxy_file, 'r')]
+        self.proxy_efficieny = {}
 
     def proxy_manager(self):
         pass
 
     def get_html(self, proxy):
+
         print "Running FF instance for proxy: {0}".format(proxy)
+
+        # Load page in Firefox and extract HTML
         driver = webdriver.Firefox()
         driver.get(self.base_url)
         source = driver.page_source
         driver.quit()
+
         return source
 
     def spawn_browsers(self):
+
+        print "\nSpawning browsers for each proxy: {0} browsers will be loaded simultaneously".format(len(self.proxy_list))
+
         for proxy in self.proxy_list:
             t = threading.Thread(target=self.calc_proxy_efficiency, args=(proxy,))
             self.threads.append(t)
             t.start()
 
     def calc_proxy_efficiency(self, proxy):
-        for x in range(0, 3):
+
+        try_count = 5
+
+        print "Will now try and load the site {0} times for proxy {1}".format(5, proxy)
+
+        # Set initial success count to 0
+        self.proxy_efficieny[proxy] = 0
+
+        # Try via proxy consecuitively 5 times in a row
+        for x in range(0, 5):
+
+            # Get the HTML of the page
             html = self.get_html(proxy)
-            print html
+
+            # If we are not on the holding page, we'll increment the success count for this proxy
+            if "holding page" not in html:
+                self.proxy_efficieny[proxy] += 1
+
+        print "\nSuccess count for each proxy:"
+        print self.proxy_efficieny
+
 
 def main():
 
