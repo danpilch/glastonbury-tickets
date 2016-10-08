d4dc258ca21ccb55c6c5e8d7d03ab68b3d29a4ee
Lewis Barber <lewis_barber@hotmail.com>
2016-10-08 12:31:27 +0100

    Fixed DO code for Python 3

--- digitalocean/proxy_generator.py
+++ digitalocean/proxy_generator.py
@@ -4,24 +4,25 @@ import time
 import os
 
 class DOInstances(object):
-    def __init__(self, instance_count, instance_region):
+    def __init__(self, instance_count, instance_region, server_prefix):
         # Get the api token from your account
         self.do_api_token = ""
         self.manager = digitalocean.Manager(token=self.do_api_token)
         self.instance_count = instance_count
         self.region = instance_region
         self.image_size = "512mb"
+        self.server_prefix = server_prefix
 
     def get_images(self):
         images = self.manager.get_my_images()
-        return images
+        return(images)
 
     def create(self, image_id):
-        print "Creating proxies"
+        print("Creating proxies")
         for instance in range(0, self.instance_count):
-            print "Creating proxy-{0}".format(instance)
+            print("Creating {!s}-{!s}".format(self.server_prefix, instance))
             droplet = digitalocean.Droplet(token=self.do_api_token,
-                    name = "proxy-{0}".format(instance),
+                    name = "{!s}-{!s}".format(self.server_prefix, instance),
                     region = self.region,
                     image = image_id,
                     size_slug = self.image_size,
@@ -29,24 +30,24 @@ class DOInstances(object):
 
             new_droplet = droplet.create()
             
-        print "Waiting for instance ip addresses to be allocated..."
+        print("Waiting for instance ip addresses to be allocated...")
         time.sleep(60)
-        print "Outputting list of proxies:\n"
+        print("Outputting list of proxies:\n")
         droplets = self.manager.get_all_droplets()
         for droplet in droplets:
-            if "proxy" in str(droplet):
-                print "user:xxjf9@{0}:9999".format(droplet.ip_address)
+            if self.server_prefix in str(droplet):
+                print("user:xxjf9@{!s}:9999".format(droplet.ip_address))
 
-        print "Complete"
+        print("Complete")
 
     def destroy(self):
-        print "Destroying proxies: "
+        print("Destroying proxies: ")
         droplets = self.manager.get_all_droplets()
         for drop in droplets:
-            if "proxy" in str(drop):
-                print "destroying: {0}".format(drop)
+            if self.server_prefix in str(drop):
+                print("destroying: {!s}".format(drop))
                 drop.destroy()
-        print "Complete"
+        print("Complete")
 
 
 def main():
@@ -61,10 +62,11 @@ def main():
     parser.add_argument("--list", "-l", help="List snapshot images available", action="store_true", required=False)
     parser.add_argument("--instances", "-i", help="Number of instances to create", required=False, default=5, type=int)
     parser.add_argument("--region", "-r", help="Region to deploy instances to", required=False, default="lon1")
+    parser.add_argument("--prefix", "-p", help="Prefix for server name", required=False, default="proxy")
     args = parser.parse_args()
 
     # Let's go
-    run = DOInstances(instance_count=args.instances, instance_region=args.region)
+    run = DOInstances(instance_count=args.instances, instance_region=args.region, server_prefix=args.prefix)
     images = run.get_images()
 
     if args.create:
@@ -73,9 +75,9 @@ def main():
         run.destroy()
     elif args.list:
         for image in images:
-            print image
+            print(image)
     else:
-        print "Doing nothing"
+        print("Doing nothing")
 
 
 if __name__ == "__main__":
